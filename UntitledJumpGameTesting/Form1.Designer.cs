d280d104bef4fb69d20b7210c82b43aa1b139c9f
Sam Collins <samcollins427@gmail.com>
2021-11-26 19:55:32 -0800

    Add project files.

---
+++ UntitledJumpGameTesting/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿
+namespace UntitledJumpGameTesting
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GenerateButton = new System.Windows.Forms.Button();
+            this.NumSidesTextbox = new System.Windows.Forms.TextBox();
+            this.NumSidesLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // GenerateButton
+            // 
+            this.GenerateButton.Location = new System.Drawing.Point(676, 12);
+            this.GenerateButton.Name = "GenerateButton";
+            this.GenerateButton.Size = new System.Drawing.Size(94, 29);
+            this.GenerateButton.TabIndex = 0;
+            this.GenerateButton.Text = "Generate";
+            this.GenerateButton.UseVisualStyleBackColor = true;
+            this.GenerateButton.Click += new System.EventHandler(this.GenerateButton_Click);
+            // 
+            // NumSidesTextbox
+            // 
+            this.NumSidesTextbox.Location = new System.Drawing.Point(98, 6);
+            this.NumSidesTextbox.Name = "NumSidesTextbox";
+            this.NumSidesTextbox.Size = new System.Drawing.Size(58, 27);
+            this.NumSidesTextbox.TabIndex = 1;
+            this.NumSidesTextbox.TextChanged += new System.EventHandler(this.NumSidesTextbox_TextChanged);
+            // 
+            // NumSidesLabel
+            // 
+            this.NumSidesLabel.AutoSize = true;
+            this.NumSidesLabel.Location = new System.Drawing.Point(12, 9);
+            this.NumSidesLabel.Name = "NumSidesLabel";
+            this.NumSidesLabel.Size = new System.Drawing.Size(80, 20);
+            this.NumSidesLabel.TabIndex = 2;
+            this.NumSidesLabel.Text = "Num Sides";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 753);
+            this.Controls.Add(this.NumSidesLabel);
+            this.Controls.Add(this.NumSidesTextbox);
+            this.Controls.Add(this.GenerateButton);
+            this.Name = "Form1";
+            this.Text = "Platform Layout";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button GenerateButton;
+        private System.Windows.Forms.TextBox NumSidesTextbox;
+        private System.Windows.Forms.Label NumSidesLabel;
+    }
+}
+
---
+++ UntitledJumpGameTesting/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UntitledJumpGameTesting
+{
+    public partial class Form1 : Form
+    {
+        //Set up values
+        private int NumSides, WindowWidth, WindowHeight = 0;
+        private Point WindowCenter;
+
+        //Simulation Params (Maybe customizable in future?)
+        //Platform Radiuses should probably scale with window height like full radius
+        private int PlatformMinDiameter = 20;
+        private int PlatformMaxDiameter = 50;
+
+        private bool DrawLayout = false;
+
+        /*--- Next Steps ---
+         - Figure out way of telling if point is in/out of bounds
+         - Create 'Platform' class or struct of something (Just to store center point + radius)
+         - Figure out way to get total area (Probably formula exists given list of vertices, maybe
+           just split into a bunch of triangles using center point/radius for height?)
+         - Figure out different amounts of platforms (low/med/high == 40/50/60 % of total area ???)
+         */
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            WindowWidth = ClientRectangle.Width;
+            WindowHeight = ClientRectangle.Height;
+            WindowCenter = new Point(WindowWidth / 2, WindowHeight / 2);
+        }
+
+        private List<Point> GetOuterPoints()
+        {
+            //By making radius 40% of height full shape will take up ~80% of window
+            var radius = (WindowHeight / 5) * 2;
+
+            List<Point> points = new List<Point>();
+
+            for (int i = 0; i < NumSides; i++)
+            {
+                var x = WindowCenter.X + (radius * Math.Cos(2 * Math.PI * i / NumSides));
+                var y = WindowCenter.Y + (radius * Math.Sin(2 * Math.PI * i / NumSides));
+                points.Add(new Point((int)x, (int)y));
+            }
+
+            return points;
+        }
+
+        private BoundingBox GetBoundingBox(List<Point> points)
+        {
+            var boundingBox = new BoundingBox
+            {
+                MinX = points.OrderBy(p => p.X).Select(p => p.X).First(),
+                MaxX = points.OrderByDescending(p => p.X).Select(p => p.X).First(),
+                MinY = points.OrderBy(p => p.Y).Select(p => p.Y).First(),
+                MaxY = points.OrderByDescending(p => p.Y).Select(p => p.Y).First()
+            };
+
+            return boundingBox;
+        }
+
+        private bool IsPointInBounds(List<Point> outerPoints, Point point)
+        {
+            //Maybe Disable BoundingBox logic here since already taken care of during generate
+            //var boundingBox = GetBoundingBox(outerPoints);
+
+            //if (point.X < boundingBox.MinX || point.X > boundingBox.MaxX
+            //    || point.Y < boundingBox.MinY || point.Y > boundingBox.MaxY)
+            //{
+            //    return false;
+            //}
+
+            bool inbounds = false;
+
+            for (int i = 0, j = outerPoints.Count - 1; i < outerPoints.Count; j = i++)
+            {
+                if ((outerPoints[i].Y > point.Y) != (outerPoints[j].Y > point.Y) &&
+                    point.X < (outerPoints[j].X - outerPoints[i].X) * (point.Y - outerPoints[i].Y) / (outerPoints[j].Y - outerPoints[i].Y) + outerPoints[i].X)
+                {
+                    inbounds = !inbounds;
+                }
+            }
+
+            return inbounds;
+        }
+
+        private List<Platform> GeneratePlatforms(List<Point> outerPoints)
+        {
+            var platforms = new List<Platform>();
+
+            var boundingBox = GetBoundingBox(outerPoints);
+
+            Random random = new Random();
+
+            int maxPlatforms = 20;
+            int currentPlatforms = 0;
+
+            while (currentPlatforms < maxPlatforms)
+            {
+                var diameter = random.Next(PlatformMinDiameter, PlatformMaxDiameter);
+                var radius = diameter / 2;
+                var centerPoint = new Point(random.Next(boundingBox.MinX, boundingBox.MaxX),
+                        random.Next(boundingBox.MinY, boundingBox.MaxY));
+
+                if (IsPointInBounds(outerPoints, centerPoint))
+                {
+                    platforms.Add(new Platform
+                    {
+                        Center = centerPoint,
+                        Diameter = diameter,
+                        Radius = radius
+                    });
+
+                    currentPlatforms++;
+                }
+            }
+
+            return platforms;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics graphics = e.Graphics;
+            Pen pen = new Pen(Color.Black, 5);
+
+            var points = GetOuterPoints();
+
+            if (points.Count > 1 && DrawLayout)
+            {
+                var platforms = GeneratePlatforms(points);
+                DrawPlatforms(graphics, platforms);
+
+                graphics.DrawPolygon(pen, points.ToArray());
+            }
+
+            graphics.Dispose();
+        }
+
+        private void DrawPlatforms(Graphics graphics, List<Platform> platforms)
+        {
+            //Pen pen = new Pen(Color.Red, 5);
+            SolidBrush brush = new SolidBrush(Color.Red);
+
+            foreach (var platform in platforms)
+            {
+                graphics.FillEllipse(brush, platform.Center.X - platform.Radius, platform.Center.Y - platform.Radius,
+                    platform.Diameter, platform.Diameter);
+            }
+        }
+
+        private void GenerateButton_Click(object sender, EventArgs e)
+        {
+            DrawLayout = true;
+            Refresh();
+        }
+
+        private void NumSidesTextbox_TextChanged(object sender, EventArgs e)
+        {
+            TextBox numSidesTextBox = (TextBox)sender;
+            if (int.TryParse(numSidesTextBox.Text, out int numSides))
+            {
+                NumSides = numSides;
+            }
+        }
+    }
+
+    public class Platform
+    {
+        public Point Center { get; set; }
+        public int Diameter { get; set; }
+        public int Radius { get; set; }
+    }
+
+    public class BoundingBox
+    {
+        public int MinX { get; set; }
+        public int MaxX { get; set; }
+        public int MinY { get; set; }
+        public int MaxY { get; set; }
+    }
+}
